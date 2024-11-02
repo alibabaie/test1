dd63d99203570ed9bca19b1c71f36140fd70f244
ali <ali_babaie2005@yahoo.com>
2024-11-02 15:34:28 +0330

    part #11

--- assets/js/useres.js
+++ assets/js/useres.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+  
+
+    const addUserBotton = document.getElementById('add_user_botton')
+    addUserBotton.addEventListener('click', ()=>{
+      document.querySelector('.add_user_modal').classList.add('show')
+      document.querySelector('.add_user_modal_back').classList.remove('dis-non')
+    })
+
+  document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
+    document.querySelector('.add_user_modal').classList.remove('show')
+    e.target.classList.add('dis-none')
+    
+  })
+
   });
