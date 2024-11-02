731b1cd09cb7efe10d1d7b06640fe63d4b5acd94
ali <ali_babaie2005@yahoo.com>
2024-11-02 18:04:49 +0330

    part #11-1

--- assets/js/useres.js
+++ assets/js/useres.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const addUserBotton = document.getElementById('add_user_botton')
     addUserBotton.addEventListener('click', ()=>{
       document.querySelector('.add_user_modal').classList.add('show')
-      document.querySelector('.add_user_modal_back').classList.remove('dis-non')
+      document.querySelector('.add_user-modal_back').classList.remove('dis-none')
     })
 
-  document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
-    document.querySelector('.add_user_modal').classList.remove('show')
-    e.target.classList.add('dis-none')
-    
-  })
+    document.querySelector('.add_user-modal_back').addEventListener('click', (e)=>{
+      document.querySelector('.add_user_modal').classList.remove('show')
+      e.target.classList.add('dis-none')
+      
+    })
 
   });
