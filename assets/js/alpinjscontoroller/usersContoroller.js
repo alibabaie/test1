bfc18e9b8c167cc72cd73da0605db68fef3335cd
ali <ali_babaie2005@yahoo.com>
2024-11-09 15:07:29 +0330

    part #20

--- assets/js/alpinjscontoroller/usersContoroller.js
+++ assets/js/alpinjscontoroller/usersContoroller.js
@@ -1,13 +1,17 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', function(){
     return {
+
         users: [],
 
-           getUsers(){
+        isLoading: false,
 
+           getUsers(){
+             this.isLoading = true
              axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
+                this.isLoading = false
                 this.users = res.data
-                console.log(res);
+                
                 
                 
              })
