303299c8699790f5668c02efd3d241006e4afef3
ali <ali_babaie2005@yahoo.com>
2024-11-10 18:41:44 +0330

    part #24

--- assets/js/alpinjscontoroller/usersContoroller.js
+++ assets/js/alpinjscontoroller/usersContoroller.js
@@ -4,15 +4,25 @@ document.addEventListener('alpine:init', () => {
 
         users: [],
 
+        pageUsers: [],
+
         isLoading: false,
 
         showAddModal: false,
+
+        pageCount: 1,
+
+        itemsCount: 4,
+
+        currentPage: 3,
 
            getUsers(){
              this.isLoading = true
              axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                 
                 this.users = res.data
+
+                this.pagination()
                 
                 this.isLoading = false
                 
@@ -23,10 +33,17 @@ document.addEventListener('alpine:init', () => {
              }).finally(()=>{
                 this.isLoading = false 
              
-             })
-             
-             
-        }
+             })    
+        },
+
+      pagination(){
+
+       this.pageCount = Math.ceil(this.users.length / this.itemsCount)  // 10/4=3
+       let start = (this.currentPage * this.itemsCount) - this.itemsCount   //1*4 - 4 =0
+       let end = this.currentPage * this.itemsCount   //1*4=4
+       this.pageUsers = this.users.slice(start,end) 
+      }
+        
     }    
           
         
